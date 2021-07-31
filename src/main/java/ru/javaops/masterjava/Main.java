2cecc234c87473c88d8e215cb8c685d960e6c989
Vladislav Arsenev <forjavatests8@gmail.com>
2021-07-31 17:42:15 +0300

    WIP

--- src/main/java/ru/javaops/masterjava/Main.java
+++ src/main/java/ru/javaops/masterjava/Main.java
@@ -1,5 +1,7 @@
 package ru.javaops.masterjava;
 
+import ru.javaops.masterjava.matrix.MatrixUtil;
+
 /**
  * User: gkislin
  * Date: 05.08.2015
@@ -10,5 +12,25 @@ package ru.javaops.masterjava;
 public class Main {
     public static void main(String[] args) {
         System.out.format("Hello MasterJava!");
+        System.out.println();
+        int matrix1[][] = new int[][] {
+                {1, 2, 8, 5},
+                {3, 6, 9, 4}
+        };
+        int matrix2[][] = new int[][]{
+                {8, 4},
+                {3, 1},
+                {4, 9},
+                {2, 5}
+        };
+
+        int matrixResult[][] = MatrixUtil.singleThreadMultiply(matrix1, matrix2);
+
+        for (int i = 0; i < matrixResult.length; i++) {
+            for (int j = 0; j < matrixResult.length; j++) {
+                System.out.print(" " + matrixResult[i][j]);
+            }
+            System.out.println();
+        }
     }
 }
